00e943bdb377ae08a078bcc947b37def42c0d768
Tomasz <dworowytomasz@gmail.com>
2019-10-26 10:29:08 +0200

    update

--- exercises/repeated_string.erl
+++ exercises/repeated_string.erl
@@ -1,5 +1,5 @@
 %% @author Tomasz
-%% @doc Lilah has a string s of lowercase English letters that she repeated infinitely many times.Given an integer n find and print the number of letter a's in the first  letters of Lilah's infinite string.
+%% @doc Lilah has a string s of lowercase English letters that she repeated infinitely many times.Given an integer n find and print the number of letter a's in the first n letters of Lilah's infinite string.
 %%s1 = "aba"
 %%n1 = 10 
 %%output 7
@@ -16,8 +16,7 @@
 %% @doc repeated string function.
 -spec(repeated_string(string(),number()) -> number()).
 repeated_string(S,N) ->
-      count_chr(S,$a) *trunc((N/length(S))) + count_chr(lists:sublist(S,1, N rem length(S)),$a).
-
+      count_chr(S,$a) * trunc((N/length(S))) + count_chr(lists:sublist(S,1, N rem length(S)),$a).
 
 
 %% @doc count character occurrence in string.
