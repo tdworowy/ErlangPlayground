c08d6577548fe257d9bdd09197d7604c5b6705ab
Tomasz <dworowytomasz@gmail.com>
2019-11-02 11:52:08 +0100

    update

--- exercises/hourglass.erl
+++ exercises/hourglass.erl
@@ -1,6 +1,6 @@
 %% @author Tomasz
 %% @doc hourglass sum
-%% input: [3, 7, -3, 0, 1, 8],[1, -4, -7, -8, -6, 5],[-8, 1, 3, 3, 5, 7],[-2, 4, 3, 1, 2, 7],[2, 4, -5, 1, 8, 4],[5, -7, 6,5, 2, 8]] 
+%% input: [[3, 7, -3, 0, 1, 8],[1, -4, -7, -8, -6, 5],[-8, 1, 3, 3, 5, 7],[-2, 4, 3, 1, 2, 7],[2, 4, -5, 1, 8, 4],[5, -7, 6,5, 2, 8]]
 %% output: 33
 
 -module(hourglass).
@@ -12,12 +12,12 @@
 hourglass_sum(Arr) ->
     Horugs = get_all_hourgs(Arr,1,1,4,2,[]),
     F = fun (Horug) ->
-        lists:sum(Horug)
+        lists:sum(tuple_to_list(Horug)) 
     end,
     Sums = lists:map(F,Horugs),
     max(Sums).
 
-get_all_hourgs(Arr,Li,I,J,Z,Horugs) when Li+2 < length(Arr) ->
+get_all_hourgs(Arr,Li,I,J,Z,Horugs) when Li+2 =< length(Arr) ->
     Sub_list1 = lists:nth(Li,Arr),
     Sub_list2 = lists:nth(Li+1,Arr),
     Sub_list3 = lists:nth(Li+2,Arr),
@@ -26,7 +26,11 @@ get_all_hourgs(Arr,Li,I,J,Z,Horugs) when Li+2 < length(Arr) ->
         X1 = slice(Sub_list1,I,J),
         X2 = lists:nth(Z,Sub_list2),
         X3 = slice(Sub_list3,I,J),
-        New_Horugs =lists:concat([Horugs,X1,X2,X3]),
+        if Horugs  /=[] ->
+            New_Horugs = lists:append(Horugs, [list_to_tuple(lists:concat([X1,X2,X3]))]);
+        true ->
+             New_Horugs = [list_to_tuple(lists:concat([X1,X2,X3]))]
+        end,
         if z < Sub_list1_lth ->
             New_Z = Z + 1;
         true ->
@@ -53,3 +57,4 @@ slice(L,Low,High)->
 slice(L,1,Low,Low,High).
 
 max([Max|T]) -> max(Max, T).
+
